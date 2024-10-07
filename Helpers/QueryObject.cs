d89c922d8dcb5db462d7960a41910ca4d4652a8c
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2024-10-07 14:54:31 +0700

    Video 19. Sorting

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -9,6 +9,9 @@ namespace Dotnet_Core_Web_API.Helpers
     {
         public string? Symbol { get; set; } = null;
         public string? Companyname { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false;
+
 
     }
 }
