34be87b131da2c0fa0788c38df7b02d539754506
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2024-10-15 10:20:24 +0700

    Video 21. Install Identity

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Dotnet_Core_Web_API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         // gõ ctor
         public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
-
+            
         }
 
         public DbSet<Stock> Stocks { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using api.Data;
 using api.Interfaces;
 using api.Responsitory;
+using Dotnet_Core_Web_API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +19,39 @@ builder.Services.AddControllers(); // đăng ký dịch vụ cho controllers tro
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
     new MySqlServerVersion(new Version(8, 0, 0)))); // Nếu không muốn chỉ định phiên bản, bạn có thể bỏ dòng này
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = true; // Yêu cầu mật khẩu có ít nhất một chữ số.
+        options.Password.RequireUppercase = true; // Yêu cầu mật khẩu có ít nhất một chữ cái viết hoa.
+        options.Password.RequireLowercase = true; // Yêu cầu mật khẩu có ít nhất một chữ cái viết thường.
+        options.Password.RequireNonAlphanumeric = true; // Yêu cầu mật khẩu có ít nhất một ký tự không phải chữ số hoặc chữ cái (ví dụ: @, #, ...).
+        options.Password.RequiredLength = 12; // Đặt độ dài tối thiểu của mật khẩu là 12 ký tự.
 
+    }
+).AddEntityFrameworkStores<ApplicationDBContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, // Kiểm tra thông tin người phát hành (issuer) của token.
+        ValidIssuer = builder.Configuration["JWT:Issuer"], // Thiết lập giá trị hợp lệ của issuer từ cấu hình (appsettings.json).
+        ValidateAudience = true, // Kiểm tra audience của token.
+        ValidAudience = builder.Configuration["JWT:Audience"], // Thiết lập giá trị hợp lệ của audience từ cấu hình.
+        ValidateIssuerSigningKey = true, // Kiểm tra khóa ký token.
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"]) // Thiết lập khóa ký để kiểm tra tính hợp lệ của token.
+        )
+    };
+});
 builder.Services.AddScoped<IStockReponsitory, StockReponsitory>(); // cấu hình Reponsitory
 builder.Services.AddScoped<ICommentReponsitory, CommentRepository>(); // cấu hình Reponsitory
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
@@ -37,5 +73,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection(); // Middleware này tự động chuyển hướng các yêu cầu HTTP sang HTTPS để đảm bảo rằng kết nối an toàn.
 
 app.MapControllers(); // định tuyến tất cả các yêu cầu HTTP đến các controller đã được định nghĩa trong ứng dụng
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.Run(); // khởi động ứng dụng 
